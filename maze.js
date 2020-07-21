ac30d168a418ac7d1afb3198f02cf9bdefde6dcf
irfan hendianto wijaya <irfanhendianto@gmail.com>
2020-07-21 23:41:05 +0700

    push pertama

---
+++ maze.js
@@ -0,0 +1,33 @@
+const draw = (number)=>{
+    let flag = 1;
+    let string = "";
+        for( let i = 1 ; i < number +1 ; i++){
+            if(i % 2 === 0){
+                for (let j = 0; j < number; j++) {
+                    (j === 0 || j === number -1) ? string += "@" : string +=" ";
+                    (j === number -1) ? string += "\n" : string +=" ";
+                } 
+            }
+            else if(flag % 2 === 0) {
+                
+                for( let j = 0; j < number; j++) {
+                    (number-2 === j) ? string +=" " : string += "@";
+                    (j === number-1) ? string += "\n" : string +=" ";
+                }
+                flag++;
+                
+            } else {
+                
+                for(let j = 0; j < number; j++) {
+                     (j === 1) ? string +=" " : string += "@";
+                     (j === number-1) ? string += "\n"  : string +=" ";
+                }
+                flag++;
+                
+            }
+        }
+    
+    return string;
+}
+
+console.log(draw(15))

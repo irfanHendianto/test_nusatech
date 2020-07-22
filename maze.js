f4552d162bf3a2fcf92d95b028f1b18697b8d13f
irfan hendianto wijaya <irfanhendianto@gmail.com>
2020-07-22 11:15:00 +0700

    Push keenam

--- maze.js
+++ maze.js
@@ -1,36 +1,29 @@
-const draw = (number)=>{
+const draw= (number)=>{
     let flag = 1;
-    let string = "";
-    if(number > 0 ){
-        for( let i = 1 ; i < number +1 ; i++){
-            if(i % 2 === 0){
-                for (let j = 0; j < number; j++) {
-                    (j === 0 || j === number -1) ? string += "@" : string +=" ";
-                    (j === number -1) ? string += "\n" : string +=" ";
-                } 
-            }
-            else if(flag % 2 === 0) {
-                
-                for( let j = 0; j < number; j++) {
-                    (number-2 === j) ? string +=" " : string += "@";
-                    (j === number-1) ? string += "\n" : string +=" ";
+    let string ="";
+    if(number > 0){
+        for (let i = 1; i < number + 1; i++) {
+            for (let j = 1; j < number + 1; j++) {
+                if( i % 2 === 0){
+                    (j === 1 || j === number) ? string += "@" : string +=" ";
+                    (j === number) ? string += "\n" : string +=" ";
+                }else if (flag % 2 === 0){
+                    (number-1 === j) ? string +=" " : string += "@";
+                    (j === number) ? (string += "\n" , flag ++)  : string +=" ";
+                }else {
+                    (j === 2) ? string +=" " : string += "@";
+                    (j === number) ?  (string += "\n" , flag ++) : string +=" ";
                 }
-                flag++;
-                
-            } else {
-                
-                for(let j = 0; j < number; j++) {
-                     (j === 1) ? string +=" " : string += "@";
-                     (j === number-1) ? string += "\n"  : string +=" ";
-                }
-                flag++;
                 
             }
+            
         }
         return string;
-    }else {
-        return "Input harus positive"
+    }else{
+        return "Input harus positive";
     }
 }
 
 console.log(draw(15))
+
+
